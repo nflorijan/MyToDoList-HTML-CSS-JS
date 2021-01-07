b00b24657cb3ef37c0aac211bc926970d2f91162
Nađ Florijan <florijan.nadj@gmail.com>
2021-01-07 22:09:10 +0100

    Adding functionality to My To Do List

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const addButton = document.getElementById('add-item-button');
+const addItemInput = document.getElementById('add-item-text');
+const itemList = document.getElementById('item-list');
+let deleteButtons = document.querySelectorAll('.list-item-button')
+
+function asignEventListeners() {
+    deleteButtons.forEach(button => button.addEventListener('click', halndeItemClickButton));
+}
+
+function getInputValue() {
+   return addItemInput.value;
+}
+
+function createNewListItem(text) {
+    const newListItem = `
+        <li class="add-item-list-item">
+            <span class="list-item-text">${text}</span>
+            <a href="#" class="list-item-button">&#10006;</a>
+        </li>
+    `;
+    return newListItem;
+}
+
+addButton.addEventListener('click', function() {
+    const inputValue = getInputValue();
+    const newListItem = createNewListItem(inputValue);
+
+    itemList.innerHTML += newListItem;
+    deleteButtons = document.querySelectorAll('.list-item-button')
+    asignEventListeners();
+});
+
+function halndeItemClickButton(event) {
+    const button = event.currentTarget;
+    const item = button.closest('.add-item-list-item');
+    item.remove();
+}
+
+asignEventListeners();
